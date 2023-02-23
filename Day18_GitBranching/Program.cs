24e060600baa9707f98e5dde8c68508ad8aeb5c0
Andrew <andrewpullen1@gmail.com>
2023-02-22 21:52:02 -0500

    made a comment

--- Day18_GitBranching/Program.cs
+++ Day18_GitBranching/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿for (int i = 0; i < 100; i++)
+{
+    for (int j = 0; j < 100; j++)
+    {
+        Console.WriteLine("O");
+    }
+
+    Console.WriteLine();
+
+}
+
+//this is my branch to work on so i son't mess up the main
