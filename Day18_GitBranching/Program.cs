b81ca522a2b7452f05e57398a5f677c8feccea20
Andrew <andrewpullen1@gmail.com>
2023-02-23 21:37:23 -0500

    corrected spelling

--- Day18_GitBranching/Program.cs
+++ Day18_GitBranching/Program.cs
@@ -11,4 +11,4 @@
 
 //git checkout -b apbranch (this is shorthand for creating a new branch and checking it out.
 
-//this is my branch to work on so i son't mess up the main
+//this is my branch to work on so i don't mess up the main
